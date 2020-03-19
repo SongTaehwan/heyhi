51b05babfd25c3e45da5a629e7f363ba6010cba0
Kim-Yonghwi <yhkim@seoulstore.com>
2020-03-19 20:43:26 +0900

    Feature: Publish main_mypage_settings_location

--- src/screens/settings/Location.tsx
+++ src/screens/settings/Location.tsx
@@ -1,11 +1,53 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Slider } from 'react-native';
+import Icon from 'react-native-vector-icons/Entypo';
+
+import { Layout, Divider, VSpace, HSpace } from '@components';
+import { Pallette } from '@styles';
+
+const styles = StyleSheet.create({
+  wrap: {
+    paddingHorizontal: 20,
+    paddingVertical: 16,
+  },
+  currentLocationWrap: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+});
 
 const Location = (): JSX.Element => {
+  const [distance, setDistance] = useState(1);
+
   return (
-    <View>
-      <Text>Location</Text>
-    </View>
+    <Layout>
+      <View style={styles.wrap}>
+        <Text>My Current Location</Text>
+        <VSpace space={32} />
+        <View style={styles.currentLocationWrap}>
+          <Icon
+            size={18}
+            name={'location-pin'}
+            color={Pallette.brightSkyBlue}
+          />
+          <HSpace space={10} />
+          <Text>Gangnam-Gu, Seoul</Text>
+        </View>
+      </View>
+      <Divider />
+      <View style={styles.wrap}>
+        <Text>Maximum Distance Setting</Text>
+        <VSpace space={70} />
+        <Slider
+          minimumValue={1}
+          maximumValue={50}
+          value={distance}
+          step={1}
+          onValueChange={setDistance}
+        />
+      </View>
+    </Layout>
   );
 };
 
