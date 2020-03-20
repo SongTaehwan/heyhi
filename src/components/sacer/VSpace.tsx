bc16179ff9d80d65208128e79f7e5d74eee5e660
SongTaehwan <axoghks@gmail.com>
2020-03-20 21:31:36 +0900

    Fix case sensitive of directories in components

--- src/components/sacer/VSpace.tsx
+++
@@ -1,9 +0,0 @@
-import { View } from 'react-native';
-import React from 'react';
-import { SpaceProp } from '@components/space/types';
-
-const VSpace: React.FC<SpaceProp> = ({ space }: SpaceProp): JSX.Element => {
-  return <View style={{ marginVertical: space / 2 }} />;
-};
-
-export default VSpace;
