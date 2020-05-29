36b67d78808e3852cd96859c7f962844960690f5
KimYongHwi <yhkim12389@gmail.com>
2020-05-29 09:30:35 +0000

    Refactor: 비밀번호 변경 부분 api 연결

--- src/screens/settings/ChangePassword.tsx
+++ src/screens/settings/ChangePassword.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/react-hooks';
+import { View, StyleSheet, Alert } from 'react-native';
+import { MEMBER } from '@api/mutation';
 import {
   ContentContainer,
   BarButton,
@@ -9,7 +10,6 @@ import {
   VSpace,
   HeadDivider,
 } from '@components';
-
 const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
@@ -18,28 +18,60 @@ const styles = StyleSheet.create({
     paddingHorizontal: 48,
   },
 });
-
 const ChnagePassword = (): JSX.Element => {
+  const [currentPassword, setCurrentPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [newPasswordConfirm, setNewPasswordConfirm] = useState('');
+  const [changePasswordMutation] = useMutation(MEMBER.CHANGE_PASSWORD, {
+    fetchPolicy: 'no-cache',
+    onCompleted: (data) => {
+      console.log('data', data.changePassword.updated);
+      if (data.changePassword.updated) {
+        Alert.alert('비밀번호가 수정 되었습니다.');
+      }
+    },
+    onError: (error) => {
+      Alert.alert(JSON.parse(error.graphQLErrors[0].message).message);
+    },
+  });
+  const changePassword = (): void => {
+    // TODO: validation
+    changePasswordMutation({
+      variables: {
+        data: { currentPassword, newPassword },
+      },
+    });
+  };
   return (
     <ContentContainer>
       <HeadDivider />
       <View style={styles.wrapper}>
         <VSpace space={40} />
-        <TextField placeholder={'Current Password'} />
-
+        <TextField
+          secureTextEntry={true}
+          value={currentPassword}
+          placeholder={'Current Password'}
+          onChangeText={setCurrentPassword}
+        />
         <VSpace space={40} />
         <Divider />
         <VSpace space={40} />
-
-        <TextField placeholder={'New Password'} />
-
+        <TextField
+          secureTextEntry={true}
+          value={newPassword}
+          placeholder={'New Password'}
+          onChangeText={setNewPassword}
+        />
         <VSpace space={20} />
-
-        <TextField placeholder={'New Password Confirm'} />
+        <TextField
+          secureTextEntry={true}
+          value={newPasswordConfirm}
+          placeholder={'New Password Confirm'}
+          onChangeText={setNewPasswordConfirm}
+        />
       </View>
-      <BarButton title="DONE" />
+      <BarButton title="DONE" onPress={changePassword} />
     </ContentContainer>
   );
 };
-
 export default ChnagePassword;
